7981f6f6774c30be620526d9e292869e3763db78
eunnam.song <sppe12@gmail.com>
2020-07-14 01:58:23 +0900

    4장 - 객체 리터널 - import 시 이름 바꾸기 - Promise 와 then 을 대신 하는 async 와 await

---
+++ primer/src/example.js
@@ -0,0 +1,12 @@
+import {asyncAdd} from "./async";
+
+let values = [10, 20, 30, 40, 50];
+
+asyncAdd(values).then( total => console.log(`Main Total: ${total}`));
+
+async function doTask() {
+    let total = await asyncAdd(values);
+    console.log(`Main Total: ${total}`);
+}
+
+doTask();
