a1deaeea8604eb11d4f37b23e971dd0716b8daa7
eunnam.song <eunnam.song@samsung.com>
2020-07-14 20:03:55 +0900

    5장 현실적인 애플리케이션 시작!!!

---
+++ sportsstore/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
