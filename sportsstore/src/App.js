ce9b598f6b85799deacb736cb165064bd9dea87f
eunnam.song <eunnam.song@samsung.com>
2020-07-15 15:34:50 +0900

    경로 매개변수는 기본적으로 대응하는 값이 꼭필요하다 이를 해결하위 해서 ? 통한 선택적 매개변수로 변경

--- sportsstore/src/App.js
+++ sportsstore/src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
         return <Provider store={SportsStoreDataStore}>
             <Router>
                 <Switch>
-                    <Route paht="/shop" component={ShopConnector}/>
+                    <Route path="/shop" component={ShopConnector}/>
                     <Redirect to="/shop"/>
                 </Switch>
             </Router>
