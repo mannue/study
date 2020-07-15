efb538eb7c90076bb79a214470d4d1da8698ae95
eunnam.song <eunnam.song@samsung.com>
2020-07-15 17:54:39 +0900

    5장 현실적인 애플리케이션 정리중!!

---
+++ sportsstore/src/ToggleLink.js
@@ -0,0 +1,29 @@
+import React, {Component, PropTypes} from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const propTypes = {};
+
+export class ToggleLink extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    static defaultProps = {};
+
+    render() {
+        return <Route path={ this.props.to } exact={ this.props.exact }
+                      children={ routeProps => {
+                          const baseClasses = this.props.className || "m-2 btn btn-block";
+                          const activeClass = this.props.activaClass || "btn-primary";
+                          const inActiveClass = this.props.inActiveClas || "btn-secondary";
+                          const combinedClasses = `${baseClasses} ${routeProps.match ? activeClass: inActiveClass}`;
+
+                          return <Link to={ this.props.to } className={ combinedClasses }>
+                              { this.props.children }
+                          </Link>
+                      }} />
+    }
+}
+
+ToggleLink.propTypes = propTypes;
