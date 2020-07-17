5fba19bfd58c9609e8de3ea692737fd917e6d207
eunnam.song <eunnam.song@samsung.com>
2020-07-17 19:00:16 +0900

    6장 데이터 로딩 컴포넌트 제

---
+++ sportsstore/src/data/DataGetter.js
@@ -0,0 +1,40 @@
+import React, {Component, Fragment, PropTypes} from 'react';
+import {DataTypes} from "./Types";
+
+const propTypes = {};
+
+export class DataGetter extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    static defaultProps = {};
+
+    render() {
+        return <Fragment>{
+            this.props.children
+        }</Fragment>
+    }
+
+    componentDidUpdate = () => this.getData();
+    componentDidMount = () => this.getData();
+
+    getData = () => {
+        const dsData = this.props.products_params || {};
+        const rtData = {
+            _limit: this.props.pageSize || 5,
+            _sort: this.props.sortKey || "name",
+            _page: this.props.match.params.page || 1,
+            category_like: (this.props.match.params.category || "") === "all" ? "" : this.props.match.params.category
+        }
+
+        if (Object.keys(rtData).find(key => dsData[key] !== rtData[key])) {
+            this.props.loadData(DataTypes.PRODUCTS, rtData);
+        }
+    }
+}
+
+
+
+DataGetter.propTypes = propTypes;
