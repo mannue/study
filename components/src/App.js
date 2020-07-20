8739261ef1f048649559d32875a121b8c339a39a
eunnam.song <sppe12@gmail.com>
2020-07-20 23:21:38 +0900

    10장 컴포넌트 와 props 정리 중!! 361 page 부터

---
+++ components/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Message} from "./Message";
+import {Summary} from "./Summary";
+
+export default function App() {
+  return (
+      <div>
+        <h1 className="bg-primary text-white text-center p-2">
+          Hello Adam
+        </h1>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Summary/>
+      </div>
+  );
+}
