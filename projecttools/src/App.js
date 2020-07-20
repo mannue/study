1623d2aa95ad1a220b1f8687cd8a66669bda4858
eunnam.song <eunnam.song@samsung.com>
2020-07-20 17:00:45 +0900

    9장 리액트 프로젝트의 이해 진행 중!!

---
+++ projecttools/src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import "./App.css";
+import reactLogo from "./logo.svg";
+
+let name = "Adam";
+const city = "London";
+
+let error = "not a valid statement";
+
+export default class App extends Component {
+    message = () => `Hello ${name} from ${city}`;
+
+    render = () =>
+        <div className="text-center">
+            <h4 className="bg-primary text-white text-center p-3">
+                { this.message() }
+            </h4>
+            <img src={ reactLogo } alt="reactLogo"/>
+            <link rel="stylesheet" href={ process.env.PUBLIC_URL + "/static.css"}/>
+            <img src={ process.env.PUBLIC_URL + "/logo.svg"} alt="reactLogo"/>
+        </div>
+}
