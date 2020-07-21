cc6f9e1d319a115b6415d66fbfea1c45577eab02
eunnam.song <sppe12@gmail.com>
2020-07-22 01:20:31 +0900

    12장 이벤트 핸들러 안에서의 컴포넌트 기능 접근 부터 하면 됨!!

---
+++ reactevents/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Ready",
+    };
+  }
+
+  handleEvent() {
+      console.log("handle Event method invoked");
+  }
+
+  render() {
+    return <div className="m-2">
+        <div className="h4 bg-primary text-white text-center p-2">
+            { this.state.message }
+        </div>
+        <div className="text-center">
+            <button className="btn btn-primary"
+                    onClick={ this.handleEvent }
+            >Click me</button>
+        </div>
+    </div>;
+  }
+}
