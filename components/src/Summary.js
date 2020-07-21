992e451534b979bc38ecb905b271247b7a71302a
eunnam.song <eunnam.song@samsung.com>
2020-07-21 15:23:36 +0900

    10장 무상태 컴포넌트 정리 끝!!

--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,19 +1,25 @@
 import React from "react";
-import {CallbackButton} from "./CallbackButton";
+import { CallbackButton } from "./CallbackButton";
 
 export function Summary(props) {
-
-    if (props.name.length >= 4) {
-        return <>
-            <td>{props.index + 1}</td>
-            <td>{props.name}</td>
-            <td>{props.name.length}</td>
-            <td>
-                <CallbackButton theme='primary' callback={props.reverseCallback} text="Reverse"/>
-                <CallbackButton theme='info' callback={() => props.promoteCallback(props.name)} text="Promote"/>
-            </td>
-        </>
-    } else {
-        return null;
-    }
+  if (props.name.length >= 4) {
+    return (
+      <>
+        <td>{props.index + 1}</td>
+        <td>{props.name}</td>
+        <td>{props.name.length}</td>
+        <td>
+          <CallbackButton callback={props.reverseCallback}/>
+          <CallbackButton
+            theme="info"
+            callback={() => props.promoteCallback(props.name)}
+            text="Promote"
+            disabled="true"
+          />
+        </td>
+      </>
+    );
+  } else {
+    return null;
+  }
 }
