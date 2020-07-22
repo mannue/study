ec556dfdfb5d5640082f3a59791b1b2f61177562
eunnam.song <sppe12@gmail.com>
2020-07-22 23:09:29 +0900

    13장 472 페이지 부

--- lifecycle/src/ActionButton.js
+++ lifecycle/src/ActionButton.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class ActionButton extends Component {
   render() {
-    console.log();
+    console.log(`Render ActionButton (${this.props.text}) Component`);
     return (
       <button className="btn btn-primary m-2" onClick={this.props.callback}>
         {this.props.text}
--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+import {Message} from "./Message";
+import List from "./List";
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+      setCounter(counter + 1);
+  }
+
+  console.log("Render App Component");
+  return <div className="container text-center">
+      <div className="row p-2">
+          <div className="col-6">
+              <Message message={ `Counter: ${counter}`}
+                       callback={incrementCounter}
+                       text="Increment Counter"/>
+          </div>
+          <div className="col-6">
+              <List/>
+          </div>
+      </div>
+  </div>
 }
 
 export default App;
