78e5466ad2f8bbd82ce717fae418527e6ed33ca5
eunnam.song <eunnam.song@samsung.com>
2020-07-22 18:03:02 +0900

    12장 정리 - 핸들러 함수를 정의하는 여러 방법, 이벤트 객체를 다루는 방법, 커스텀 인자 (함수 인자가 추가) - 캡쳐 , 타깃, 버블 단계로 이벤트가 발생

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -1,27 +1,78 @@
 import React, { Component } from "react";
+import {ThemeButton} from "./ThemeButton";
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       message: "Ready",
+      counter: 0,
+      theme: "secondary",
     };
   }
 
-  handleEvent() {
-      console.log("handle Event method invoked");
+  selectTheme = (newTheme) => {
+    this.setState({
+        theme: newTheme,
+        message: `Theme: ${newTheme}`
+    })
+  }
+
+  handleEvent = (event, newTheme) => {
+    event.persist();
+    this.setState(
+      {
+        counter: this.state.counter + 1,
+        theme: newTheme,
+      },
+      () =>
+        this.setState({
+          message: `${event.type}: ${this.state.counter}`,
+        })
+    );
+  };
+
+  toggleCheckBox = (event) => {
+    if (this.state.counter === 0) event.preventDefault();
+  };
+
+  handleClick = (event) => {
+      console.log(`App: Type: ${event.type} `
+      + `Target: ${event.target.tagName} `
+      + `CurrentTarget: ${event.currentTarget.tagName}`);
   }
 
   render() {
-    return <div className="m-2">
-        <div className="h4 bg-primary text-white text-center p-2">
-            { this.state.message }
+    return (
+      <div className="m-2" onClick={this.handleClick }>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onClick={this.toggleCheckBox}
+          />
+          <label>This is a checkbox</label>
         </div>
-        <div className="text-center">
-            <button className="btn btn-primary"
-                    onClick={ this.handleEvent }
-            >Click me</button>
+        <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
+          {this.state.message}
         </div>
-    </div>;
+        <div className="text-center" onClick={ this.handleClick }>
+            <ThemeButton theme="primary" callback={this.selectTheme}/>
+            <ThemeButton theme="danger" callback={this.selectTheme} onClick={this.handleClick}/>
+          <button
+            className="btn btn-primary"
+            onClick={(e) => this.handleEvent(e, "primary")}
+          >
+            Normal
+          </button>
+          <button
+            className="btn btn-danger m-1"
+            onClick={(e) => this.handleEvent(e, "danger")}
+          >
+            Danger
+          </button>
+        </div>
+      </div>
+    );
   }
 }
