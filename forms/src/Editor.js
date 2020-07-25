1797f5b34036577125e5a3e2b19bf7b3554141e9
eunnam.song <sppe12@gmail.com>
2020-07-26 04:17:54 +0900

    15장 566 page 부터 시작!

---
+++ forms/src/Editor.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Editor extends Component {
+    render() {
+        return (
+            <div className="h5 bg-info text-white p-2">
+                Form Will Go Here
+            </div>
+        );
+    }
+}
+
+export default Editor;
