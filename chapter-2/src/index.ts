0746a59e948eb4802e2f6ff2c277f1197847674d
eunnam.song <eunnam.song@samsung.com>
2020-08-04 19:30:34 +0900

    2장 완료!!

--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -1,1 +1,11 @@
-console.log("test")
+// 연습문제
+// 1번
+let a = 1 + 2
+let b = a + 3
+let c = {
+    apple: a,
+    banana: b
+}
+let d = c.apple * 4
+
+console.log(`type : ${typeof d}, value : ${d}`)
