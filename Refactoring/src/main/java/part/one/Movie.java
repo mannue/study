09556a71e8f348b1c31c9482e73efedc9d5e60a0
mannue <sppe12@gmail.com>
2021-01-07 00:44:11 +0900

    State/Strategy Pattern 완

--- Refactoring/src/main/java/part/one/Movie.java
+++ Refactoring/src/main/java/part/one/Movie.java
@@ -6,7 +6,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         this._title = title;
@@ -14,11 +14,16 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR -> _price = new RegularPrice();
+            case NEW_RELEASE -> _price = new NewReleasePrice();
+            case CHILDREN -> _price = new ChildrensPrice();
+            default -> throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
@@ -26,26 +31,10 @@ public class Movie {
     }
 
     double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR -> {
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-            }
-            case Movie.NEW_RELEASE -> result += daysRented * 3;
-            case Movie.CHILDREN -> {
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-            }
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
--- Refactoring/src/main/java/part/one/Price.java
+++ Refactoring/src/main/java/part/one/Price.java
@@ -2,4 +2,8 @@ package part.one;
 
 abstract class Price {
     abstract int getPriceCode();
+    abstract double getCharge(int daysRented);
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
