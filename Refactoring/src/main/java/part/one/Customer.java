fc9ebf4dbe7269183a84aaba107edbbc4951761b
mannue <sppe12@gmail.com>
2021-01-06 22:57:47 +0900

    query method 로 추출

--- Refactoring/src/main/java/part/one/Customer.java
+++ Refactoring/src/main/java/part/one/Customer.java
@@ -23,23 +23,36 @@ public class Customer {
     // 요구사항 변경이 복잡하게 변경되어야 한다.
     //
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental)rentals.nextElement();
-
-            // 포인트 (frequent renter points) 추가
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
             // 이 대여에 대한 요금계산 결과 표시
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
         // 풋터(footer) 추가
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + "frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getFrequentRenterPoints()) + "frequent renter points";
         return  result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
     }
 }
