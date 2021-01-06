3de8c67627e0ae0e067fa89ebf6b7e93a7a5ee1f
mannue <sppe12@gmail.com>
2021-01-06 23:29:37 +0900

    movie class 로 요금계산과 포인트 계산 역할 변경

--- Refactoring/src/main/java/part/one/Movie.java
+++ Refactoring/src/main/java/part/one/Movie.java
@@ -24,4 +24,28 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR -> {
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+            }
+            case Movie.NEW_RELEASE -> result += daysRented * 3;
+            case Movie.CHILDREN -> {
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+            }
+        }
+        return result;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        return 1;
+    }
 }
--- Refactoring/src/main/java/part/one/Rental.java
+++ Refactoring/src/main/java/part/one/Rental.java
@@ -18,29 +18,10 @@ public class Rental {
     }
 
     public double getCharge() {
-        double result = 0;
-        switch (_movie.getPriceCode()) {
-            case Movie.REGULAR -> {
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-            }
-            case Movie.NEW_RELEASE -> result += getDaysRented() * 3;
-            case Movie.CHILDREN -> {
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-            }
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        int frequentRenterPoints = 0;
-        frequentRenterPoints++;
-        // 최신(new release) 을 이틀 이상 대여하는 경우 추가 포인트 제공
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            frequentRenterPoints ++;
-        return frequentRenterPoints;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
