56680fd1e3c9cce896ca2b7254c0d32eb1f00a92
mannue <Tmsen25*)>
2021-05-30 16:16:17 +0900

    설명 추가

--- studyGroup/JumpGame2.js
+++ studyGroup/JumpGame2.js
@@ -8,14 +8,16 @@ var jump = function(nums) {
 };
 
 function jumpData(numList, startIndex, jump,keep, count) {
+    // index 와 점프 값이 배열 길이보다 크거나 같으면 종료한다.
     if (numList.length-1 <= (startIndex + jump)) {
         return count + 1;
     } else {
         let maxIndex = 0;
         let max = 0;
+        // keep 은 그전 계산된 범위에 있었다면 해당 범위는 무시하고 그 다음 범위를 계산한다.
         for (let i=1+keep; i <= jump; i++) {
             let value = numList[startIndex+i] + startIndex+i;
-            if (value > max) {
+            if (value > max) {  // 점프 범위 안에서 인덱스와 점프 값을 더해서 최대값을 구한다.
                 max = value;
                 maxIndex = startIndex + i;
             }
