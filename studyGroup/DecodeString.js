5e7560afbecd08aaa95a06ff9afc46b4ae704227
mannue <Tmsen25*)>
2021-07-18 18:54:56 +0900

    394. Decode String

---
+++ studyGroup/DecodeString.js
@@ -0,0 +1,43 @@
+var decodeString = function(s) {
+    let numberStack = [];
+    let stringStack = [];
+    let result = "";
+    let numberValue = "";
+    for (let value of s) {
+        if (value > '/' && value < ':') {
+            numberValue = numberValue + value
+        }
+        else {
+            if (numberValue !== "") {
+                numberStack.push(numberValue);
+                numberValue = "";
+            }
+            switch (value) {
+                case ']':
+                    let res = getString(stringStack);
+                    stringStack.push(res.repeat(numberStack.pop()));
+                    break;
+                default:
+                    stringStack.push(value);
+            }
+        }
+    }
+    for (let item of stringStack) {
+        result = result + item;
+    }
+    return result;
+};
+
+function getString(stack) {
+    let res = "";
+    let size = stack.length;
+    for(let i=0; i < size; i++) {
+        let s = stack.pop();
+        if (s === '[') break;
+        else res = s + res;
+    }
+    return res;
+}
+let rs = decodeString("100[leetcode]")
+console.log('a'.repeat(2));
+
