0a61fee99473c83b7ecb063acbf1b55709247e24
mannue <Tmsen25*)>
2021-08-19 11:45:54 +0900

    149. Max Points on a Line

--- studyGroup/MaxPointsOnaLine.js
+++ studyGroup/MaxPointsOnaLine.js
@@ -1,34 +1,27 @@
 var maxPoints = function(points) {
     if (points.length <= 2) return points.length;
-    let x_table = {};
-    let y_table = {};
     let res = 0;
     for (let i=0; i < points.length; i++) {
         let up_diagonal = {};
         let down_diagonal = {};
         let select = null;
-        x_table[`${points[i][0]}`]  = x_table[`${points[i][0]}`] ? x_table[`${points[i][0]}`] + 1 : 1;
-        y_table[`${points[i][1]}`]  = y_table[`${points[i][1]}`] ? y_table[`${points[i][1]}`] + 1 : 1;
-        res = Math.max(res,);
         for (let j=i+1; j < points.length; j++) {
             let maxX = Math.max(points[i][0],points[j][0]);
             let maxY = Math.max(points[i][1],points[j][1]);
             let minX = Math.min(points[i][0],points[j][0]);
             let minY = Math.min(points[i][1],points[j][1]);
             let key = Math.atan2((maxY-minY),(maxX-minX));
-
             if ((points[i][0] > points[j][0] && points[i][1] > points[j][1]) || ((points[i][0] < points[j][0] && points[i][1] < points[j][1]))) {
                 select = down_diagonal;
             } else {
                 select = up_diagonal;
             }
             select[key] = select[key] ? select[key] + 1 : 2;
-            res = Math.max(res,select[key],x_table[`${points[i][0]}`],y_table[`${points[i][1]}`])
+            res = Math.max(res,select[key])
         }
     }
     return res;
 };
-
 let res = maxPoints(
-    [[-6,-1],[3,1],[12,3]])
+    [[0,0],[1,-1],[1,1]])
 console.log(res)
